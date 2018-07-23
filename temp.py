58532c09583ef87946112689cd51a58850487746
s17003 <s17003@std.it-college.ac.jp>
2018-07-23 14:41:19 +0900

    commit

--- temp.py
+++ temp.py
@@ -1,7 +1,10 @@
-def exec():
-    pass
+def exec(s):
+    return s
 
 def display(f, *args):
     print(f(*args))
 
 if __name__ == "__main__":
+    s = input()
+    identity = lambda x: x
+    display(identity, exec(s))
