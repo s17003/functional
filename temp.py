72d58ca5d65958080b2dfb52364774d90e684450
s17003 <s17003@std.it-college.ac.jp>
2018-07-23 16:54:34 +0900

    234 commit

--- temp.py
+++ temp.py
@@ -1,10 +1,13 @@
-def exec(s):
-    return s
+import operator as op
+from functools import partial
+
+def exec(a, b, c):
+    f = lambda x: op.add(1,x)
+    return map(partial(op.add, 1), [a, b, c])
 
 def display(f, *args):
     print(f(*args))
 
 if __name__ == "__main__":
-    s = input()
-    identity = lambda x: x
-    display(identity, exec(s))
+    a, b, c = [int(input()) for _ range(3)]
+    display('\n'.join, exec(a, b, c))
